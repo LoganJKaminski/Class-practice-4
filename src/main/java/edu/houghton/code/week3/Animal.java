16a523ceee50093aef05fbe13e9bdeb7b9b10289
LoganJKaminski <logan.kaminski28@houghton.edu>
2025-01-30 03:29:13 -0500

    Task 4 done

--- src/main/java/edu/houghton/code/week3/Animal.java
+++ src/main/java/edu/houghton/code/week3/Animal.java
@@ -5,6 +5,67 @@ package edu.houghton.code.week3;
  * Course: CSCI 218 - Programming II
  */
 public class Animal {
-
+    private float mass;
+    private String name;
+    private int legs;
+    private boolean isAlive;
+    
+    public static int counter = 0;
+    public static int animalsAlive = 0;
+    
+    public Animal(String name) {
+        this.name = name;
+        isAlive = true;
+        animalsAlive++;
+        counter++;
+        legs = 0;
+        mass = 0f;
+    }
+    
+    public Animal(float mass, String name, int legs) {
+        this.mass = mass;
+        this.name = name;
+        this.legs = legs;
+        isAlive = true;
+        animalsAlive++;
+        counter++;
+    }
+    
+    public void setMass(float mass) {
+        this.mass = mass;
+    }
+    
+    public float getMass() {
+        return mass;
+    }
+    public boolean getIsAlive() {
+        return isAlive;
+    }
+    public int getLegs() {
+        return legs;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    public String print() {
+        return "animal (name=" + getName() + ", mass=" + getMass()
+                + ", legs=" + getLegs() + ", " + "status=" + (getIsAlive() ? "Alive" : "Dead");
+    }
+    
+    public static int getAnimalPopulation() {
+        return counter;
+    }
+    public static int getAnimalsAlive() {
+        return animalsAlive;
+    }
+    
+    public Animal reproduce() {
+        return new Animal(getMass()/2.0f, getName(), getLegs());
+    }
+    public void die() {
+        isAlive = false;
+        animalsAlive--;
+    }
 
 }
--- src/main/java/edu/houghton/code/week3/TestAnimal.java
+++ src/main/java/edu/houghton/code/week3/TestAnimal.java
@@ -6,4 +6,37 @@ package edu.houghton.code.week3;
  */
 
 public class TestAnimal {
+    public static void main(String[] args) {
+        Animal snail = new Animal(3.5f, "snail", 1);
+        System.out.println("Population: " + Animal.getAnimalPopulation()
+            + ", Animals alive: " + Animal.getAnimalsAlive());
+        Animal cat = new Animal("cat");
+        System.out.println(cat.print());
+        System.out.println("Population: " + Animal.getAnimalPopulation()
+            + ", Animals alive: " + Animal.getAnimalsAlive());
+        cat.setMass(4.2f);
+        Animal dog = new Animal(2.1f, "dog", 4);
+        System.out.println(dog.print());
+        System.out.println("Population: " + Animal.getAnimalPopulation()
+            + ", Animals alive: " + Animal.getAnimalsAlive());
+        Animal babyAnimal1;
+        Animal babyAnimal2;
+        babyAnimal1 = cat.reproduce();
+        babyAnimal2 = dog.reproduce();
+        snail.die();
+        System.out.println("Population: " + Animal.getAnimalPopulation()
+            + ", Animals alive: " + Animal.getAnimalsAlive());
+        babyAnimal1.counter = 500;
+        System.out.println("Population: " + babyAnimal2.getAnimalPopulation()
+            + ", Animals alive: " + babyAnimal2.getAnimalsAlive());
+        Animal.counter = 15;
+        System.out.println("Population: " + babyAnimal2.getAnimalPopulation()
+            + ", Animals alive: " + babyAnimal2.getAnimalsAlive());
+        cat.die();
+        dog.die();
+        babyAnimal1.die();
+        babyAnimal2.die();
+        System.out.println("Population: " + babyAnimal2.getAnimalPopulation()
+            + ", Animals alive: " + babyAnimal2.getAnimalsAlive());
+    }
 }
