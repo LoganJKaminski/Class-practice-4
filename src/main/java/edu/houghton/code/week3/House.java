d81a1ab69f72665918af7071d895e280547bc769
LoganJKaminski <logan.kaminski28@houghton.edu>
2025-01-30 04:20:04 -0500

    Finished task 6

--- src/main/java/edu/houghton/code/week3/House.java
+++ src/main/java/edu/houghton/code/week3/House.java
@@ -1,5 +1,7 @@
 package edu.houghton.code.week3;
 
+import java.util.Objects;
+
 /**
  * Name : (Your name here)
  * Course: CSCI 218 - Programming II
@@ -7,7 +9,7 @@ package edu.houghton.code.week3;
  * Todo:
  *
  * Private instance variables to store age of the House, its type (Detached, Semi-Attached, Attached) and its cost.
- *
+ * 
  * 4 constructors: No argument (sets age to 50, type to Attached and cost to 100000), one argument constructor (sets cost to a value, age - 50 and type - Attached), two argument constructors (sets age to a value, cost to a value , and type to Attached), three argument constructors (sets age to a value, cost to a value, and type to Attached, semi-detached, or detached)
  *
  * 3 Accessor/Getter methods: - methods to return age, type and cost respectively
@@ -29,37 +31,154 @@ package edu.houghton.code.week3;
 public class House {
 
     //declare your variables
-
+    private double cost;
+    private int age;
+    private String type;
 
     //create your constructors
-
+    public House() {
+        this.cost = 100000.0;
+        this.age = 50;
+        this.type = "Attached";
+    }
+    public House(double cost) {
+        this.cost = cost;
+        this.age = 50;
+        this.type = "Attached";
+    }
+    public House(double cost, int age) {
+        this.cost = cost;
+        this.age = age;
+        this.type = "Attached";
+    }
+    public House(double cost, int age, String type) {
+        this.cost = cost;
+        this.age = age;
+        this.type = type;
+    }
 
     //create your getter methods
-
+    public double getCost() {
+        return cost;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getType() {
+        return type;
+    }
 
     //Create the setter methods
-
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setHouse(double cost, int age) {
+        this.cost = cost;
+        this.age = age;
+    }
+    public void setHouse(double cost, int age, String type) {
+        this.cost = cost;
+        this.age = age;
+        this.type = type;
+    }
 
 
     //create your estimatePrice method
-
+    public double estimatePrice() {
+        if(getType().equals("Attached")) {
+            if(age > 5) {
+                return ((100000.0)*((1.01)*5))*((1.02)*(getAge()-5));
+            } else {
+                return (100000.0)*((1.01)*getAge());
+            }
+        } else if(getType().equals("Semi-Attached")) {
+            if(age > 5) {
+                return ((150000.0)*((1.02)*5))*((1.03)*(getAge()-5));
+            } else {
+                return (150000.0)*((1.02)*getAge());
+            }
+        } else if(getType().equals("Detached")) {
+            return (200000.0)*((1.02)*getAge());
+        } else {
+            return 0;
+        }
+    }
 
 
     //create the toString() method
     //to do this, right click -> generate -> toString
-
+    @Override
+    public String toString() {
+        return "House{" + "cost=" + cost + ", age=" + age + ", type=" + type + '}';
+    }
 
 
     //create the equals() method
     //to do this, right click -> generate -> override methods - > equals()
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final House other = (House) obj;
+        if (this.age != other.age) {
+            return false;
+        }
+        return Objects.equals(this.type, other.type);
+    }
 
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + this.age;
+        hash = 83 * hash + Objects.hashCode(this.type);
+        return hash;
+    }
 
 
     //create the isLessThan() method
     //this will be similar to how your equals method definition looks like
-
+    public boolean isLessThan(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final House other = (House) obj;
+        return cost < other.getCost();
+    }
 
 
     //create the isGreaterThan() method
     //this will be similar to how your equals method definition looks like
+    public boolean isGreaterThan(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final House other = (House) obj;
+        return cost > other.getCost();
+    }
+
 }
--- src/main/java/edu/houghton/code/week3/HouseDriver.java
+++ src/main/java/edu/houghton/code/week3/HouseDriver.java
@@ -19,6 +19,45 @@ package edu.houghton.code.week3;
 public class HouseDriver {
 
     public static void main(String[] args) {
-
+        House h1 = new House();
+        House h2 = new House(50000.0);
+        House h3 = new House(90000.0, 50);
+        House h4 = new House(20000.0, 8, "Detached");
+        System.out.println(h1);
+        System.out.println(h2);
+        System.out.println(h3);
+        System.out.println(h4);
+        
+        System.out.println();
+        System.out.println("Accessor Method: The housetype for house H4 is " + h4.getType()
+            + ", its age is " + h4.getAge() + ", and it costs " + h4.getCost());
+        
+        System.out.println("");
+        System.out.println("The estimated price of house h1 is " + h1.estimatePrice());
+        System.out.println("The estimated price of house h4 is " + h4.estimatePrice());
+        
+        System.out.println("");
+        h1.setAge(5);
+        System.out.println("Mutator Method: the new age for house h1 is " + h1.getAge());
+        h1.setType("Semi-Attached");
+        System.out.println("Mutator Method: the new type for house h1 is " + h1.getType());
+        h1.setCost(240000.0);
+        System.out.println("Mutator Method: the new cost for house h1 is " + h1.getCost());
+        h1.setHouse(150000.0, 10);
+        System.out.println("Mutator Method: the new cost for house h1 is " + h1.getCost() + " and its new age is " + h1.getCost());
+        h1.setHouse(10000.0, 40, "Attached");
+        System.out.println("Mutator Method: the new cost for house h1 is " + h1.getCost() + " and its new age is " + h1.getCost() + " and its new type is " + h1.getType());
+        
+        System.out.println("");
+        System.out.println("toString: " + h1.toString());
+        
+        System.out.println("");
+        System.out.println("Houses H2 and H3 are equal is " + h2.equals(h3));
+        System.out.println("Houses H1 and H2 are equal is " + h1.equals(h2));
+        
+        System.out.println("");
+        System.out.println("House H2 is greater than H3 is " + h2.isGreaterThan(h3));
+        System.out.println("House H4 is less than H3 is " + h4.isLessThan(h3));
+        
     }
 }
